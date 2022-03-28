fe9bbb75f7ef366f703b02e67f05f6f625f96467
Ratson <github@ratson.name>
2022-03-28 22:18:55 +0800

    Group tests

--- __tests__/node.ts
+++ __tests__/node.ts
@@ -4,24 +4,28 @@ import { execa } from "execa";
 const fixturesDir = (...args: string[]) =>
   path.join("./test/fixtures", ...args);
 
-test("return false for non-main ESM", async () => {
-  const { stdout } = await execa("node", [fixturesDir("non-main.mjs")]);
-  expect(stdout).toBe("");
+describe("ESM", () => {
+  test("return false for non-main ESM", async () => {
+    const { stdout } = await execa("node", [fixturesDir("non-main.mjs")]);
+    expect(stdout).toBe("");
+  });
+
+  test("return true for main ESM", async () => {
+    const { stdout } = await execa("node", [fixturesDir("main.mjs")]);
+    expect(stdout).toBe("is main");
+  });
 });
 
-test("return true for main ESM", async () => {
-  const { stdout } = await execa("node", [fixturesDir("main.mjs")]);
-  expect(stdout).toBe("is main");
-});
+describe("CJS", () => {
+  test("return false for non-main module", async () => {
+    const { stdout } = await execa("node", [fixturesDir("non-main.cjs")]);
+    expect(stdout).toBe("");
+  });
 
-test("return false for non-main module", async () => {
-  const { stdout } = await execa("node", [fixturesDir("non-main.cjs")]);
-  expect(stdout).toBe("");
-});
-
-test("return true for main module", async () => {
-  const { stdout } = await execa("node", [fixturesDir("main.cjs")]);
-  expect(stdout).toBe("is main");
+  test("return true for main module", async () => {
+    const { stdout } = await execa("node", [fixturesDir("main.cjs")]);
+    expect(stdout).toBe("is main");
+  });
 });
 
 describe("--experimental-modules", () => {
